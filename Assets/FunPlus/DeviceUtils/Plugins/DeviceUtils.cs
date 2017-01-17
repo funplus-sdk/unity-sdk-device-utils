640cd05a1c814056dd3213dc8b75648301c4f01e
yuankun.zhang <fmcocoa@gmail.com>
2017-01-17 15:37:49 +0800

    initial commit

---
+++ Assets/FunPlus/DeviceUtils/Plugins/DeviceUtils.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FunPlus.DeviceUtils
+{
+
+	public class DeviceUtils
+	{
+
+		public static string getIdentifierForVendor()
+		{
+		}
+
+		public static string getAdvertisingIdentifier()
+		{
+		}
+
+		public static string getAndroidId()
+		{
+		}
+
+		public static string getPlayAdvertisingId()
+		{
+		}
+
+		public static string getDeviceType()
+		{
+		}
+
+		public static string getModelName()
+		{
+		}
+
+		public static string getManufacturer()
+		{
+		}
+
+		public static string getSystemName()
+		{
+		}
+
+		public static string getSystemVersion()
+		{
+		}
+
+		public static string getAndroidApiLevel()
+		{
+		}
+
+		public static string getAppName()
+		{
+		}
+
+		public static string getAppVersion()
+		{
+		}
+
+		public static string getAppLanguage()
+		{
+		}
+
+		public static string getNetworkCarrierName()
+		{
+		}
+
+		public static bool setScreenBrightness(float brightness)
+		{
+		}
+	}
+
+}
